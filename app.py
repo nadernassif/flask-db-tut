82591541dbcd3dedef3e289b2ec81f41b1ae0387
Nader Youssef <nader.youssef@eficode.com>
2022-12-06 14:03:11 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, flash, url_for, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+app.config['SECRET_KEY'] = '3bce79429ad6b79670e4e800fb4a57b9'
+
+db = SQLAlchemy(app)
+
+class students(db.Model):
+   id = db.Column('id', db.Integer, primary_key = True)
+   name = db.Column(db.String(100))
+   city = db.Column(db.String(50))
+   addr = db.Column(db.String(200)) 
+   pin = db.Column(db.String(10))
+   __tablename__ = 'students'
+   __table_args__ = {'extend_existing': True}
+   
+   def __init__(self, name, city, addr, pin):
+      self.name = name
+      self.city = city
+      self.addr = addr
+      self.pin = pin
+
+@app.route('/')
+def show_all():
+   return render_template('show_all.html', students = students.query.all() )
+
+@app.route('/new', methods = ['GET', 'POST'])
+def new():
+   if request.method == 'POST':
+      if not request.form['name'] or not request.form['city'] or not request.form['addr']:
+         flash('Please enter all the fields', 'error')
+      else:
+         student = students(request.form['name'], request.form['city'],
+            request.form['addr'], request.form['pin'])
+         db.session.add(student)
+         db.session.commit()
+         flash('Record was successfully added')
+         return redirect(url_for('show_all'))
+   return render_template('new.html')
+
+if __name__ == '__main__':
+   db.create_all()
+   app.run(debug = True)
